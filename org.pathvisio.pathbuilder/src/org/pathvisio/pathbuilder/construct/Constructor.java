8bd3295dd924284290af33a75209cce33a49ec75
Christ Leemans <christleemans@gmail.com>
2013-06-12 22:01:58 +0200

    created different packages

---
+++ org.pathvisio.pathbuilder/src/org/pathvisio/pathbuilder/construct/Constructor.java
@@ -0,0 +1,5 @@
+package org.pathvisio.pathbuilder.construct;
+//TODO: This package should take care of putting the nodes and edges on the screen in a structurized manner
+public class Constructor {
+
+}
