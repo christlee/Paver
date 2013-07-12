97b9f48c2389a8a2c132a66aafce24821a6a2a5a
christlee <christleemans@gmail.com>
2013-07-12 03:18:05 +0200

    added layout function

--- org.pathvisio.pathbuilder/src/org/pathvisio/pathbuilder/PbPlugin.java
+++ org.pathvisio.pathbuilder/src/org/pathvisio/pathbuilder/PbPlugin.java
@@ -1,7 +1,6 @@
 package org.pathvisio.pathbuilder;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.io.File;
 
@@ -9,54 +8,80 @@ import javax.swing.AbstractAction;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 
-import org.pathvisio.core.model.Property;
-import org.pathvisio.core.model.PropertyManager;
-import org.pathvisio.core.preferences.GlobalPreference;
-import org.pathvisio.core.preferences.Preference;
-
-import org.bridgedb.DataSource;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.pathvisio.core.preferences.Preference;
 import org.pathvisio.desktop.PvDesktop;
 import org.pathvisio.desktop.plugin.Plugin;
-import org.pathvisio.gui.handler.PropertyView;
+import org.pathvisio.pathbuilder.layout.ISOM;
 
 public class PbPlugin implements Plugin, BundleActivator {
 
 	private static String PLUGIN_NAME = "PathBuilder";
+	private static String BUILD = "Build Pathway";
+	private static String LAYOUT = "Layout";
 	
-	private selectAction action;
+	
+	private selectAction build;
+	private selectAction layout;
 	private JFrame frame;
 	private PvDesktop desktop;
 	private JMenu subMenu;
 	private static BundleContext context;
 
+	public static enum Action
+	{
+		BUILD,
+		LAYOUT;
+	}
+	
 	@Override
 	public void init(PvDesktop desktop) {
 		this.desktop = desktop;
-		action = new selectAction();
+		build = new selectAction(Action.BUILD);
+		layout = new selectAction(Action.LAYOUT);
 		subMenu = new JMenu();
 		subMenu.setText(PLUGIN_NAME);
-		subMenu.add(action);
+		subMenu.add(build);
+		subMenu.add(layout);
 		desktop.registerSubMenu("Plugins", subMenu);
 	}
 	
 	private class selectAction extends AbstractAction {
 
-		selectAction()
+		private Action action;
+		selectAction(Action action)
 		{
-			putValue(NAME,"Build pathway");
+			this.action = action;
+			switch(action)
+			{
+			case BUILD:
+				putValue(NAME,BUILD);
+				break;
+			case LAYOUT:
+				putValue(NAME,LAYOUT);
+				break;
+			}
 		}
 		@Override
 		public void actionPerformed(ActionEvent arg0) 
 		{
-			frame = new JFrame("Build pathway");
+			switch(action)
+			{
+			case BUILD:
+				frame = new JFrame("Build pathway");
 			
-			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			frame.getContentPane().add(new InputWindow(desktop,frame), BorderLayout.CENTER);
-			frame.pack();
-			frame.setLocationRelativeTo(desktop.getFrame());
-			frame.setVisible(true);
+				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				frame.getContentPane().add(new InputWindow(desktop,frame), BorderLayout.CENTER);
+				frame.pack();
+				frame.setLocationRelativeTo(desktop.getFrame());
+				frame.setVisible(true);
+				break;
+			case LAYOUT:
+				new ISOM(desktop.getSwingEngine());
+				break;
+			}
+			
 			
 		}
 		
---
+++ org.pathvisio.pathbuilder/src/org/pathvisio/pathbuilder/layout/ISOM.java
@@ -0,0 +1,34 @@
+package org.pathvisio.pathbuilder.layout;
+
+import java.awt.geom.Point2D;
+
+import org.apache.commons.collections15.Transformer;
+import org.pathvisio.gui.SwingEngine;
+
+import edu.uci.ics.jung.algorithms.layout.AbstractLayout;
+import edu.uci.ics.jung.algorithms.layout.ISOMLayout;
+
+public class ISOM extends LayoutAbstract{
+
+	ISOMLayout<String,String> l; 
+	Transformer<String,Point2D> in;
+	
+	
+	public ISOM(SwingEngine swingEngine){
+		super.swingEngine = swingEngine;
+		super.pwy = swingEngine.getEngine().getActivePathway();
+		
+		createDSMultigraph();
+		l = new ISOMLayout<String,String>( g );
+		setDimension(l);
+		l.initialize();
+		while(!l.done()){
+			l.step();
+		}
+		
+		drawNodes((AbstractLayout<String,String>) l);
+		//re-draw the lines
+		drawLines();
+	}
+
+}
